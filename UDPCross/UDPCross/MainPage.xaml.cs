1caedcb33ef44527a46a3b60878dcf5b6e72030d
Robson Correia <robsoncorreia01@outlook.com>
2018-11-25 11:40:59 -0200

    Add project files.

---
+++ UDPCross/UDPCross/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace UDPCross
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram,
+            ProtocolType.Udp);
+
+            IPAddress broadcast = IPAddress.Parse("192.168.1.109");
+
+            byte[] sendbuf = Encoding.ASCII.GetBytes("oi");
+            IPEndPoint ep = new IPEndPoint(broadcast, 1234);
+
+            s.SendTo(sendbuf, ep);
+
+            Console.WriteLine("Message sent to the broadcast address");
+        }
+    }
+}
